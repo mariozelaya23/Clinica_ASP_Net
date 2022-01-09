283e1f7b255ecd8d08200188ee3a408d5afd0af6
Mario Zelaya <mariozelaya23@gmail.com>
2022-01-08 20:22:54 -0800

    adding SQL server and connec string to web.config

--- Clinica_ASP_Net/Pacientes.aspx.cs
+++ Clinica_ASP_Net/Pacientes.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
 
 namespace Clinica_ASP_Net
 {
@@ -13,5 +15,7 @@ namespace Clinica_ASP_Net
         {
 
         }
+
+        SqlConnection con = new SqlConnection();
     }
 }
